78bb226db8c6512afbce2395b3084be3ef66318a
Bob <pavlakis.stratos@gmail.com>
2017-06-12 23:15:51 +0300

    third commit

--- src/utils.js
+++ src/utils.js
@@ -9,9 +9,15 @@ export function capitalize(str) {
   return str.replace(/\b\w/g, l => l.toUpperCase());
 }
 
-export function palindrome(str) {  
+export function palindrome(str) {
   const re = /[\W_]/g; // or const re = /[^A-Za-z0-9]/g;
   const lowRegStr = str.toLowerCase().replace(re, '');
   const reverseStr = lowRegStr.split('').reverse().join('');
   return reverseStr === lowRegStr; // "amanaplanacanalpanama" === "amanaplanacanalpanama"? => true
+}
+
+export function isEmpty(str) {
+  return str === null || str === undefined
+    ? true
+    : /^[\s\xa0]*$/.test(str);
 }
